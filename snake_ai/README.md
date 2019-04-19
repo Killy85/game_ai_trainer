d6dcb8fe4b3fe7ffbfda07bfde8790d98cf6c5dd
Benjamin Brasseur <benjamin.brasseur85@gmail.com>
2019-04-19 02:20:37 +0200

    Updating Agent and Game program. And Adding a basic 'how to' documentation

---
+++ snake_ai/README.md
@@ -0,0 +1,26 @@
+# Installation et lancement du jeu
+
+## Requirements
+
+- pygame
+- keras
+- tensorflow
+- PLE (PyGame Learning Environment)
+
+### Installation de tensorflow
+
+> Ne fonctionne que sur Linux
+
+(Guide d'installation)[https://www.tensorflow.org/install/pip]
+
+### Installation de PLE
+
+```
+git clone https://github.com/ntasfi/PyGame-Learning-Environment
+cd PyGame-Learning-Environment
+pip install -e .
+```
+
+## Lancement du jeu
+
+`python snake_game.py`
--- snake_ai/agent.py
+++ snake_ai/agent.py
@@ -2,12 +2,19 @@ import numpy as np
 from keras.models import Sequential, load_model
 from keras.layers.core import Dense, Activation
 from keras.optimizers import Adam, sgd
+import random
+import time
 import os
 
+from collections import deque
+
+
 class Trainer:
-    def __init__(self, allowed_actions, name=None, learning_rate=0.01, epsilon_decay=0.9999):
+
+    def __init__(self, allowed_actions, height, width, name=None, learning_rate=0.01, epsilon_decay=0.9999):
         self.action_size = len(allowed_actions)
         self.actions = allowed_actions
+        self.state_size = 400
         self.gamma = 0.9
         self.epsilon = 1.0
         self.epsilon_min = 0.01
@@ -25,6 +32,39 @@ class Trainer:
 
         self.model = model
 
-    def pickAction(self, reward, observation):
+    def pickAction(self, state, rand=True):
 
-        return self.actions[0]
+        self.epsilon *= self.epsilon_decay
+
+        if rand and np.random.rand() <= self.epsilon:
+            # The agent acts randomly
+            return random.randrange(self.action_size)
+
+        # Predict the reward value based on the given state
+        act_values = self.model.predict(np.array([state]))
+
+        # Pick the action based on the predicted reward
+        action =  np.argmax(act_values[0])  
+
+        return action
+
+    def getSelectedAction(self, action):
+        return self.actions[action]
+
+    def train(self, state, action, reward, next_state, done):
+        target = self.model.predict(np.array([state]))[0]
+        if done:
+            target[action] = reward
+        else:
+            target[action] = reward + self.gamma * np.max(self.model.predict(np.array([next_state])))
+
+        inputs = np.array([state])
+        outputs = np.array([target])
+
+        return self.model.fit(inputs, outputs, epochs=1, verbose=0, batch_size=1)
+
+    def save(self):
+        if self.name:
+            self.model.save("model-" + self.name, overwrite=True)
+        else:
+            self.model.save("model-" + str(time.time()))
