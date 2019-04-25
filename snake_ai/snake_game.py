bbcb1bcd53cd8a34210cfaf7559d931ab2db8528
Benjamin Brasseur <benjamin.brasseur85@gmail.com>
2019-04-26 01:06:56 +0200

    Adding reward method

--- snake_ai/snake_game.py
+++ snake_ai/snake_game.py
@@ -1,4 +1,5 @@
 import sys, pygame
+import random
 
 class Snake:
 
@@ -16,22 +17,28 @@ class Snake:
     '''
         Initialisation du jeu
     '''
-    def __init__(self, nb_cases):
+    def __init__(self, nb_cases, display=True):
         pygame.init()
-        screen_size = nb_cases * self.CASE_SIZE
-        size = width, height = screen_size, screen_size
+        self.display = display
+        self.screen_size = nb_cases * self.CASE_SIZE
+        size = width, height = self.screen_size, self.screen_size
         self.white = 255, 255, 255
         self.screen = pygame.display.set_mode(size)
         self.score = 0
 
         self.players = []
-        add_player_body()
+        self.add_player_body()
         player = self.players[0]
-        player['player_rect'].move_ip(player['actual_position'])
+        player_image = pygame.image.load("img/player_case.gif")
+        player_rect = player_image.get_rect()
+        player_rect.move_ip(player['actual_position'])
         self.screen.fill(self.white)
-        self.screen.blit(self.player, self.player_rect)
+        self.screen.blit(player_image, player_rect)
 
-        pygame.display.flip()
+        self.food = [random.randrange(),random.randrange()]
+
+        if(self.display):
+            pygame.display.flip()
 
 
     '''
@@ -44,19 +51,21 @@ class Snake:
         '''
             Déplacement des body en fonction du body "parent"
         '''
-        for i in range(self.players.len() - 1, 0):
+        for i in range(len(self.players) - 1, 0):
             body = self.players[i]
+            body_player = pygame.image.load("img/player_case.gif")
+            body_rect = body_player.get_rect()
             self.screen.fill(self.white)
-            self.screen.blit(body['player_image'], body['player_rect'])
+            self.screen.blit(body_player, body_rect)
 
         '''
             Déplacemement du player en fonction de l'action, de sa position et de sa direction
         '''
         player = self.players[0]
         direction = player['actual_direction']
-        if(action == self.ACTIONS.LEFT):
+        if(action == self.ACTIONS[1]):
             
-            if(direction == 'UP')
+            if(direction == 'UP'):
                 player['actual_direction'] = 'LEFT'
             elif(direction == 'LEFT'):
                 player['actual_direction'] = 'DOWN'
@@ -65,9 +74,9 @@ class Snake:
             elif(direction == 'RIGHT'):
                 player['actual_direction'] = 'UP'
 
-        elif(action == self.ACTIONS.RIGHT):
+        elif(action == self.ACTIONS[2]):
             
-            if(direction == 'UP')
+            if(direction == 'UP'):
                 player['actual_direction'] = 'RIGHT'
             elif(direction == 'LEFT'):
                 player['actual_direction'] = 'UP'
@@ -76,45 +85,77 @@ class Snake:
             elif(direction == 'RIGHT'):
                 player['actual_direction'] = 'DOWN'
 
+        player['actual_position'][0] += self.DIRECTIONS[player['actual_direction']][0]
+        player['actual_position'][1] += self.DIRECTIONS[player['actual_direction']][1]
 
-        player['actual_position'] = player['actual_position'] + DIRECTIONS[player['actual_direction']]
-        player['player_rect'].move_ip(player['actual_position'])
+        player_image = pygame.image.load("img/player_case.gif")
+        player_rect = player_image.get_rect()
+        player_rect.move_ip(player['actual_position'])
+
         self.screen.fill(self.white)
-        self.screen.blit(body['player_image'], body['player_rect'])
+        self.screen.blit(player_image, player_rect)
 
-        pygame.display.flip()
+        if(self.display):
+            pygame.display.update()
 
-        reward = get_reward()
+        reward = self.get_reward()
 
-        return {state:player['actual_position'], reward: reward}
+        if(reward > 0):
+            
+
+        return {'state': player['actual_position'], 'reward': reward}
 
     def score():
         return self.score
 
     def add_player_body(self):
-        nb_players = self.players.len()
+        nb_players = len(self.players)
         body_architecture = {
             'actual_position': [0,0],
-            'actual_direction': 'LEFT',
-            'player_image': pygame.image.load("img/player_case.gif"),
-            'player_rect': body_architecture['player_image'].get_rect()
+            'actual_direction': 'LEFT'
         }
+
         if(nb_players > 0):
             last_body = self.players[nb_players - 1]
-            free = get_body_free_space(nb_players - 1)
+            free = self.get_body_free_space(nb_players - 1)
             if(free > 1):
+                print('RANDOM')
+            else:
+                print('ONLY')
         else:
-            body_architecture['actual_position'] = [screen_size/2, screen_size/2]
+            body_architecture['actual_position'] = [self.screen_size/2, self.screen_size/2]
 
         self.players.append(body_architecture)
 
+    def get_food_position(self):
+        x = random.randrange(0, self.screen_size, self.CASE_SIZE)
+        y = random.randrange(0, self.screen_size, self.CASE_SIZE)
+        return 
 
     def get_body_free_space(self, body_index):
         free = self.ACTIONS
-        for i in range(self.players.len()):
-            if(i != body_index ):
+        for i in range(len(self.players)):
+            if(i != body_index):
                 print('poof')
         return free
 
     def get_reward(self):
-        return 0
+        player = self.players[0]
+        reward = 0
+
+        pos = player['actual_position']
+        if(pos[0] == self.food[0] && pos[1] == self.food[1]):
+            # Check if player touch food
+            reward = 1
+        elif(pos[0] < 0 || pos[0] > self.screen_size || pos[1] < 0 || pos[1] > self.screen_size):
+            # Check if player is on the border
+            reward = -1
+        else:
+            # Check if player touch hiself
+            for i in range(self.players):
+                pos_body = self.players[i]['actual_position']
+                if(i != body_index && (pos_body[0] == pos[0] && pos_body[1] == pos[1])):
+                    reward = -1
+                    break
+
+        return reward
