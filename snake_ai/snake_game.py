e0b8524fe772c6d18629c1a5ec18e35ff8821444
Benjamin Brasseur <benjamin.brasseur85@gmail.com>
2019-04-26 16:38:30 +0200

    Adding body parts and food

--- snake_ai/snake_game.py
+++ snake_ai/snake_game.py
@@ -24,22 +24,8 @@ class Snake:
         size = width, height = self.screen_size, self.screen_size
         self.white = 255, 255, 255
         self.screen = pygame.display.set_mode(size)
-        self.score = 0
-
-        self.players = []
-        self.add_player_body()
-        player = self.players[0]
-        player_image = pygame.image.load("img/player_case.gif")
-        player_rect = player_image.get_rect()
-        player_rect.move_ip(player['actual_position'])
-        self.screen.fill(self.white)
-        self.screen.blit(player_image, player_rect)
-
-        self.food = [random.randrange(),random.randrange()]
-
-        if(self.display):
-            pygame.display.flip()
-
+        
+        self.reset()
 
     '''
         Déplace la tête et le reste du corps en fonction de l'action du joueur
@@ -48,14 +34,17 @@ class Snake:
         new_position = [0,0]
         reward = 0
 
+        self.screen.fill(self.white)
+
         '''
             Déplacement des body en fonction du body "parent"
         '''
-        for i in range(len(self.players) - 1, 0):
-            body = self.players[i]
+        for i in range(1, len(self.players)):
+            body = self.players[len(self.players)-i]
+            
             body_player = pygame.image.load("img/player_case.gif")
             body_rect = body_player.get_rect()
-            self.screen.fill(self.white)
+            body_rect.move_ip(body['actual_position'])
             self.screen.blit(body_player, body_rect)
 
         '''
@@ -88,24 +77,38 @@ class Snake:
         player['actual_position'][0] += self.DIRECTIONS[player['actual_direction']][0]
         player['actual_position'][1] += self.DIRECTIONS[player['actual_direction']][1]
 
+        self.players[0] = player
+
         player_image = pygame.image.load("img/player_case.gif")
         player_rect = player_image.get_rect()
         player_rect.move_ip(player['actual_position'])
 
-        self.screen.fill(self.white)
         self.screen.blit(player_image, player_rect)
-
-        if(self.display):
-            pygame.display.update()
 
         reward = self.get_reward()
 
         if(reward > 0):
-            
+            self.score += 1
+            self.add_player_body()
+            self.food = self.get_food_position()
 
-        return {'state': player['actual_position'], 'reward': reward}
+            print(body_architecture)
 
-    def score():
+        self.show_food()
+
+        if(self.display):
+            pygame.display.flip()
+
+        return {'snake_location': player['actual_position'], 'food_location': self.food, 'reward': reward}
+
+    def show_food(self):
+        food_image = pygame.image.load("img/food_case.gif")
+        food_rect = food_image.get_rect()
+        food_rect.move_ip(self.food)
+        self.screen.blit(food_image, food_rect)
+
+
+    def get_score(self):
         return self.score
 
     def add_player_body(self):
@@ -118,25 +121,46 @@ class Snake:
         if(nb_players > 0):
             last_body = self.players[nb_players - 1]
             free = self.get_body_free_space(nb_players - 1)
-            if(free > 1):
-                print('RANDOM')
-            else:
-                print('ONLY')
+            key, value = random.choice(list(free.items()))
+            body_architecture['actual_position'] = value
         else:
-            body_architecture['actual_position'] = [self.screen_size/2, self.screen_size/2]
+            body_architecture['actual_position'] = [int(self.screen_size/2), int(self.screen_size/2)]
 
         self.players.append(body_architecture)
 
     def get_food_position(self):
-        x = random.randrange(0, self.screen_size, self.CASE_SIZE)
-        y = random.randrange(0, self.screen_size, self.CASE_SIZE)
-        return 
+        x = int(random.randrange(0, self.screen_size, self.CASE_SIZE))
+        y = int(random.randrange(0, self.screen_size, self.CASE_SIZE))
+        x = 60
+        y = 100
+        return [x,y]
 
     def get_body_free_space(self, body_index):
-        free = self.ACTIONS
+        pos = self.players[body_index]['actual_position']
+        up    = [pos[0]                             , pos[1] + self.DIRECTIONS['UP'][1]]
+        left  = [pos[0] + self.DIRECTIONS['LEFT'][0], pos[1]]
+        down  = [pos[0]                             , pos[1] + self.DIRECTIONS['DOWN'][1]]
+        right = [pos[0] + self.DIRECTIONS['RIGHT'][0], pos[1]]
+
+        free = {}
+
+        # On contrôle d'abord les bordures
+        if(self.control_border(up)):
+            free['UP'] = up
+        elif(self.control_border(left)):
+            free['LEFT'] = left
+        elif(self.control_border(down)):
+            free['DOWN'] = down
+        elif(self.control_border(right)):
+            free['RIGHT'] = right
+
         for i in range(len(self.players)):
+            pos = self.players[i]['actual_position']
             if(i != body_index):
-                print('poof')
+                for key in free:
+                    coord = free[key]
+                    if(coord[0] == pos[0] and coord[1] == pos[1]):
+                        del free[key]
         return free
 
     def get_reward(self):
@@ -144,18 +168,47 @@ class Snake:
         reward = 0
 
         pos = player['actual_position']
-        if(pos[0] == self.food[0] && pos[1] == self.food[1]):
+        if(pos[0] == self.food[0] and pos[1] == self.food[1]):
             # Check if player touch food
             reward = 1
-        elif(pos[0] < 0 || pos[0] > self.screen_size || pos[1] < 0 || pos[1] > self.screen_size):
+        elif(pos[0] < 0 or pos[0] >= self.screen_size or pos[1] < 0 or pos[1] >= self.screen_size):
             # Check if player is on the border
             reward = -1
         else:
             # Check if player touch hiself
-            for i in range(self.players):
+            for i in range(len(self.players)):
                 pos_body = self.players[i]['actual_position']
-                if(i != body_index && (pos_body[0] == pos[0] && pos_body[1] == pos[1])):
+                if(i != 0 and (pos_body[0] == pos[0] and pos_body[1] == pos[1])):
                     reward = -1
                     break
 
         return reward
+
+    def reset(self):
+        self.score = 0
+
+        self.players = []
+
+        self.add_player_body()
+
+        player = self.players[0]
+        player_image = pygame.image.load("img/player_case.gif")
+        player_rect = player_image.get_rect()
+        player_rect.move_ip(player['actual_position'])
+
+        self.screen.fill(self.white)
+        self.screen.blit(player_image, player_rect)
+
+        self.food = self.get_food_position()
+
+        self.show_food()
+
+        if(self.display):
+            pygame.display.flip()
+
+    def control_border(self, pos):
+        in_game_screen = True
+        if(pos[0] < 0 or pos[0] >= self.screen_size):
+            in_game_screen = False
+
+        return in_game_screen
---
+++ snake_ai/snake_runner.py
@@ -0,0 +1,67 @@
+from ple.games.snake import Snake
+from ple import PLE
+from agent import Trainer
+import datetime
+
+flatten = lambda l: [item for sublist in l for item in sublist]
+
+def get_state(game, snake_location, food_location, alea=False):
+    x, y = snake_location
+    if alea:
+        return [get_grille(game, x, y) for (x, y) in
+                [snake_location, food_location]]
+    return flatten(get_grille(game, x, y))
+
+def get_grille(game, x, y):
+    grille = [
+        [0] * int(game.width/10) for i in range(int(game.height/10))
+    ]
+    grille[x][y] = 1
+    return grille
+
+
+# NE PAS CHANGER CETTE VARIABLE
+case_size = 20
+size = 10
+
+# Initialisation du jeu
+game = Snake(height=case_size*size,width=case_size*size)
+p = PLE(game, fps=30, display_screen=True)
+
+agent = Trainer(allowed_actions=p.getActionSet(), height=game.height, width=game.width)
+
+p.init()
+reward = 0.0
+nb_frames = 10000000000000000
+bestScore = 0
+
+for i in range(nb_frames):
+
+    if(p.score() > bestScore):
+        bestScore = int(p.score())
+        print('New Best Score : '+str(bestScore) + ' a ' + str(datetime.datetime.now()))
+
+    if p.game_over():
+        p.reset_game()
+
+    observation = p.getGameState()
+    food_location = [int(observation.get('food_x')/10), int(observation.get('food_y')/10)]
+    snake_location = [int(observation.get('snake_head_x')/10), int(observation.get('snake_head_y')/10)]
+    state = get_state(game, snake_location, food_location)
+    action = agent.pickAction(state, True)
+    reward = p.act(agent.getSelectedAction(action))
+
+    # Fixing Reward by punishing more the player when he is not doing the right thing.
+    if(reward == -1): # If he loose (By touching himself or the border)
+        reward = -100
+    elif(reward == 1): # If he get the food
+        reward = 200
+    
+    observation = p.getGameState()
+    food_location = [int(observation.get('food_x')/10), int(observation.get('food_y')/10)]
+    snake_location = [int(observation.get('snake_head_x')/10), int(observation.get('snake_head_y')/10)]
+    next_state = get_state(game, snake_location, food_location)
+
+    agent.train(state, action, reward, next_state, False)
+
+print('Best Score : '+str(bestScore))
